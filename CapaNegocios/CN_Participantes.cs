ca29a24e456f6b0cb9ab4174205211a31736fa70
Aleng <Aleng@ALENGUNI>
2026-02-27 23:54:04 -0400

    Actualizacion con metodo Editar

--- CapaNegocios/CN_Participantes.cs
+++ CapaNegocios/CN_Participantes.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CapaDatos;
-using System.Data;
-using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CapaNegocios
 {
@@ -13,18 +14,42 @@ namespace CapaNegocios
     {
         private CD_Conexion conexion = new CD_Conexion();
 
-        SqlDataReader leer;
-        DataTable tabla = new DataTable();
-        SqlCommand comando = new SqlCommand();
-
         public DataTable MostrarParticipantes()
         {
+            SqlCommand comando = new SqlCommand();
+            DataTable tabla = new DataTable();
             comando.Connection = conexion.AbrirConexion();
-            comando.CommandText = "Select * From Participante";
-            leer = comando.ExecuteReader();
+            comando.CommandText = "sp_ObtenerParticipantes";
+            SqlDataReader leer = comando.ExecuteReader();
             tabla.Load(leer);
             conexion.CerrarConexion();
             return tabla;
+        }
+
+        public void Insertar( string nombre, string apellido, string cedula, string correo, string telefono)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "INSERT INTO Participante (Matricula, Nombre, Apellido, Cedula, Correo, Telefono) VALUES ('Temp', '" + nombre + "', '" + apellido + "', '" + cedula + "', '" + correo + "', '" + telefono + "')";
+            comando.CommandType = CommandType.Text;
+            comando.ExecuteNonQuery();
+            conexion.CerrarConexion();
+
+        }
+
+        public void Editar(string matricula, string nombre, string apellido, string cedula, string correo, string telefono)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarParticipante";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@Matricula", matricula);
+            comando.Parameters.AddWithValue("@Nombre", nombre);
+            comando.Parameters.AddWithValue("@Apellido", apellido);
+            comando.Parameters.AddWithValue("@Cedula", cedula);
+            comando.Parameters.AddWithValue("@Correo", correo);
+            comando.Parameters.AddWithValue("@Telefono", telefono);
+            comando.ExecuteNonQuery();
         }
     }
 
@@ -36,6 +61,16 @@ namespace CapaNegocios
             DataTable tabla = new DataTable();
             tabla = objeto.MostrarParticipantes();
             return tabla;
+        }
+
+        public void InsertarEstudiante(string nombre, string apellido, string cedula, string correo, string telefono)
+        {
+            objeto.Insertar(nombre,apellido,cedula,correo,telefono);
+        }
+
+        public void EditarParticipante(string matricula, string nombre, string apellido, string cedula, string correo, string telefono)
+        {
+            objeto.Editar(matricula, nombre, apellido, cedula, correo, telefono);
         }
     }
 
--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            txtNombre = new TextBox();
+            txtApellido = new TextBox();
+            txtCedula = new TextBox();
+            txtCorreo = new TextBox();
+            txtTelefono = new TextBox();
+            btnInsertar = new Button();
+            btnEditar = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -38,24 +45,99 @@
             dataGridView1.Location = new Point(26, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(723, 427);
+            dataGridView1.Size = new Size(593, 376);
             dataGridView1.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(978, 68);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(258, 27);
+            txtNombre.TabIndex = 1;
+            txtNombre.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(978, 129);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(258, 27);
+            txtApellido.TabIndex = 2;
+            txtApellido.Text = "Apellido";
+            // 
+            // txtCedula
+            // 
+            txtCedula.Location = new Point(978, 189);
+            txtCedula.Name = "txtCedula";
+            txtCedula.Size = new Size(258, 27);
+            txtCedula.TabIndex = 3;
+            txtCedula.Text = "Cedula";
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Location = new Point(978, 241);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(258, 27);
+            txtCorreo.TabIndex = 4;
+            txtCorreo.Text = "Correo";
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(978, 300);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(258, 27);
+            txtTelefono.TabIndex = 5;
+            txtTelefono.Text = "Telefono";
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(978, 370);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(258, 65);
+            btnInsertar.TabIndex = 7;
+            btnInsertar.Text = "Guardar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            btnInsertar.Click += btnInsertar_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(26, 406);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(94, 29);
+            btnEditar.TabIndex = 8;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Click += btnEditar_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1267, 501);
+            Controls.Add(btnEditar);
+            Controls.Add(btnInsertar);
+            Controls.Add(txtTelefono);
+            Controls.Add(txtCorreo);
+            Controls.Add(txtCedula);
+            Controls.Add(txtApellido);
+            Controls.Add(txtNombre);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtCedula;
+        private TextBox txtCorreo;
+        private TextBox txtTelefono;
+        private Button btnInsertar;
+        private Button btnEditar;
     }
 }
