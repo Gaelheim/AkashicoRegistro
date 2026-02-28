ace84a725b83c4509a1cef5c7cb125b9fa344859
Aleng <Aleng@ALENGUNI>
2026-02-27 23:54:04 -0400

    Actualizacion con metodo Editar

--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            txtNombre = new TextBox();
+            txtApellido = new TextBox();
+            txtCedula = new TextBox();
+            txtCorreo = new TextBox();
+            txtTelefono = new TextBox();
+            btnInsertar = new Button();
+            btnEditar = new Button();
+            btnBajaA = new Button();
+            btnBajaM = new Button();
+            btnActivar = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -38,24 +48,135 @@
             dataGridView1.Location = new Point(26, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(723, 427);
+            dataGridView1.Size = new Size(869, 376);
             dataGridView1.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(978, 68);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(258, 27);
+            txtNombre.TabIndex = 1;
+            txtNombre.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(978, 129);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(258, 27);
+            txtApellido.TabIndex = 2;
+            txtApellido.Text = "Apellido";
+            // 
+            // txtCedula
+            // 
+            txtCedula.Location = new Point(978, 189);
+            txtCedula.Name = "txtCedula";
+            txtCedula.Size = new Size(258, 27);
+            txtCedula.TabIndex = 3;
+            txtCedula.Text = "Cedula";
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Location = new Point(978, 241);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(258, 27);
+            txtCorreo.TabIndex = 4;
+            txtCorreo.Text = "Correo";
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(978, 300);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(258, 27);
+            txtTelefono.TabIndex = 5;
+            txtTelefono.Text = "Telefono";
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(978, 370);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(258, 65);
+            btnInsertar.TabIndex = 7;
+            btnInsertar.Text = "Guardar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            btnInsertar.Click += btnInsertar_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(26, 406);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(94, 29);
+            btnEditar.TabIndex = 8;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnBajaA
+            // 
+            btnBajaA.Location = new Point(166, 406);
+            btnBajaA.Name = "btnBajaA";
+            btnBajaA.Size = new Size(139, 29);
+            btnBajaA.TabIndex = 9;
+            btnBajaA.Text = "Baja Academica";
+            btnBajaA.UseVisualStyleBackColor = true;
+            btnBajaA.Click += btnBajaA_Click;
+            // 
+            // btnBajaM
+            // 
+            btnBajaM.Location = new Point(166, 441);
+            btnBajaM.Name = "btnBajaM";
+            btnBajaM.Size = new Size(139, 29);
+            btnBajaM.TabIndex = 10;
+            btnBajaM.Text = "Baja Medica";
+            btnBajaM.UseVisualStyleBackColor = true;
+            btnBajaM.Click += btnBajaM_Click;
+            // 
+            // btnActivar
+            // 
+            btnActivar.Location = new Point(354, 406);
+            btnActivar.Name = "btnActivar";
+            btnActivar.Size = new Size(139, 29);
+            btnActivar.TabIndex = 11;
+            btnActivar.Text = "Activar";
+            btnActivar.UseVisualStyleBackColor = true;
+            btnActivar.Click += btnActivar_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1267, 532);
+            Controls.Add(btnActivar);
+            Controls.Add(btnBajaM);
+            Controls.Add(btnBajaA);
+            Controls.Add(btnEditar);
+            Controls.Add(btnInsertar);
+            Controls.Add(txtTelefono);
+            Controls.Add(txtCorreo);
+            Controls.Add(txtCedula);
+            Controls.Add(txtApellido);
+            Controls.Add(txtNombre);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtCedula;
+        private TextBox txtCorreo;
+        private TextBox txtTelefono;
+        private Button btnInsertar;
+        private Button btnEditar;
+        private Button btnBajaA;
+        private Button btnBajaM;
+        private Button btnActivar;
     }
 }
--- CapaPresentacion/Form1.cs
+++ CapaPresentacion/Form1.cs
@@ -5,7 +5,9 @@ namespace CapaPresentacion
 {
     public partial class Form1 : Form
     {
-        CN_Participantes estudiante = new CN_Participantes();
+        Estudiantes estudiante = new Estudiantes();
+        private string matricula = null;
+        private bool Editar = false;
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +20,117 @@ namespace CapaPresentacion
 
         private void Mostrarestudiante()
         {
-            dataGridView1.DataSource = estudiante.MostrarParticipantes();
+            dataGridView1.DataSource = estudiante.MostrarEstudiantes();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (Editar == false)
+            {
+
+                try
+                {
+                    estudiante.InsertarEstudiante(txtNombre.Text, txtApellido.Text, txtCedula.Text, txtCorreo.Text, txtTelefono.Text);
+                    MessageBox.Show("Se guardaron los datos correctamente!");
+                    Mostrarestudiante();
+                    LimpiarForm();
+                    Editar = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudieron guardar los datos por: " + ex);
+                }
+            }
+
+            if (Editar == true)
+            {
+                try
+                {
+                    estudiante.EditarParticipante(matricula, txtNombre.Text, txtApellido.Text, txtCedula.Text, txtCorreo.Text, txtTelefono.Text);
+                    MessageBox.Show("Se editaron los datos correctamente!");
+                    Mostrarestudiante();
+                    LimpiarForm();
+                    Editar = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudieron editar los datos por: " + ex);
+                }
+
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Editar = true;
+                txtNombre.Text = dataGridView1.CurrentRow.Cells["Nombre"].Value.ToString();
+                txtApellido.Text = dataGridView1.CurrentRow.Cells["Apellido"].Value.ToString();
+                txtCedula.Text = dataGridView1.CurrentRow.Cells["Cedula"].Value.ToString();
+                txtCorreo.Text = dataGridView1.CurrentRow.Cells["Correo"].Value.ToString();
+                txtTelefono.Text = dataGridView1.CurrentRow.Cells["Telefono"].Value.ToString();
+                matricula = dataGridView1.CurrentRow.Cells["Matricula"].Value.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila, por favor");
+            }
+        }
+
+        private void LimpiarForm()
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtCedula.Clear();
+            txtCorreo.Clear();
+            txtTelefono.Clear();
+
+        }
+
+        private void btnBajaA_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                matricula = dataGridView1.CurrentRow.Cells["Matricula"].Value.ToString();
+                estudiante.CambiarEstatusA(matricula);
+                MessageBox.Show("Estatus modificado con exito");
+                Mostrarestudiante();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila, por favor");
+            }
+        }
+
+        private void btnBajaM_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                matricula = dataGridView1.CurrentRow.Cells["Matricula"].Value.ToString();
+                estudiante.CambiarEstatusM(matricula);
+                MessageBox.Show("Estatus modificado con exito");
+                Mostrarestudiante();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila, por favor");
+            }
+        }
+
+        private void btnActivar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                matricula = dataGridView1.CurrentRow.Cells["Matricula"].Value.ToString();
+                estudiante.ActivarEstudiante(matricula);
+                MessageBox.Show("Estatus modificado con exito");
+                Mostrarestudiante();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila, por favor");
+            }
         }
     }
 }
