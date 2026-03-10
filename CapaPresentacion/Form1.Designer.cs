623504f0e96bd4d40b0e2ba249cac1fced16841d
Aleng <Aleng@ALENGUNI>
2026-03-09 20:59:26 -0400

    restricciones aplicadas al form participantes

--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -43,6 +43,11 @@
             btnPrograma = new Button();
             btnDocentes = new Button();
             btnInscripciones = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -50,61 +55,65 @@
             // 
             dataGridView1.BackgroundColor = SystemColors.GradientInactiveCaption;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 148);
+            dataGridView1.Location = new Point(11, 170);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(723, 427);
+            dataGridView1.Size = new Size(905, 427);
             dataGridView1.TabIndex = 0;
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(964, 204);
+            txtNombre.Location = new Point(967, 171);
             txtNombre.MaxLength = 50;
             txtNombre.Name = "txtNombre";
             txtNombre.Size = new Size(258, 27);
             txtNombre.TabIndex = 1;
-            txtNombre.Text = "Nombre";
+            txtNombre.KeyPress += txtNombre_KeyPress;
             // 
             // txtApellido
             // 
-            txtApellido.Location = new Point(964, 265);
+            txtApellido.Location = new Point(967, 232);
             txtApellido.MaxLength = 50;
             txtApellido.Name = "txtApellido";
             txtApellido.Size = new Size(258, 27);
             txtApellido.TabIndex = 2;
-            txtApellido.Text = "Apellido";
+            txtApellido.KeyPress += txtApellido_KeyPress;
             // 
             // txtCedula
             // 
-            txtCedula.Location = new Point(964, 325);
+            txtCedula.Location = new Point(967, 292);
             txtCedula.MaxLength = 20;
             txtCedula.Name = "txtCedula";
             txtCedula.Size = new Size(258, 27);
             txtCedula.TabIndex = 3;
-            txtCedula.Text = "Cedula";
+            txtCedula.TextChanged += txtCedula_TextChanged;
+            txtCedula.KeyPress += txtCedula_KeyPress;
             // 
             // txtCorreo
             // 
-            txtCorreo.Location = new Point(964, 377);
+            txtCorreo.Location = new Point(967, 344);
             txtCorreo.MaxLength = 90;
             txtCorreo.Name = "txtCorreo";
             txtCorreo.Size = new Size(258, 27);
             txtCorreo.TabIndex = 4;
-            txtCorreo.Text = "Correo";
+            txtCorreo.Text = "Ejemplo@correo.com";
+            txtCorreo.TextChanged += txtCorreo_TextChanged;
+            txtCorreo.KeyPress += txtCorreo_KeyPress;
+            txtCorreo.Leave += txtCorreo_Leave;
             // 
             // txtTelefono
             // 
-            txtTelefono.Location = new Point(964, 436);
+            txtTelefono.Location = new Point(967, 403);
             txtTelefono.MaxLength = 15;
             txtTelefono.Name = "txtTelefono";
             txtTelefono.Size = new Size(258, 27);
             txtTelefono.TabIndex = 5;
-            txtTelefono.Text = "Telefono";
+            txtTelefono.KeyPress += txtTelefono_KeyPress;
             // 
             // btnInsertar
             // 
             btnInsertar.BackColor = Color.PaleGreen;
-            btnInsertar.Location = new Point(964, 506);
+            btnInsertar.Location = new Point(967, 473);
             btnInsertar.Name = "btnInsertar";
             btnInsertar.Size = new Size(258, 65);
             btnInsertar.TabIndex = 7;
@@ -115,9 +124,9 @@
             // btnEditar
             // 
             btnEditar.BackColor = SystemColors.ActiveCaption;
-            btnEditar.Location = new Point(12, 581);
+            btnEditar.Location = new Point(967, 566);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(94, 29);
+            btnEditar.Size = new Size(258, 64);
             btnEditar.TabIndex = 8;
             btnEditar.Text = "Editar";
             btnEditar.UseVisualStyleBackColor = false;
@@ -126,7 +135,7 @@
             // btnBajaA
             // 
             btnBajaA.BackColor = SystemColors.ActiveCaption;
-            btnBajaA.Location = new Point(152, 612);
+            btnBajaA.Location = new Point(11, 603);
             btnBajaA.Name = "btnBajaA";
             btnBajaA.Size = new Size(139, 29);
             btnBajaA.TabIndex = 9;
@@ -137,7 +146,7 @@
             // btnBajaM
             // 
             btnBajaM.BackColor = SystemColors.ActiveCaption;
-            btnBajaM.Location = new Point(152, 577);
+            btnBajaM.Location = new Point(156, 603);
             btnBajaM.Name = "btnBajaM";
             btnBajaM.Size = new Size(139, 29);
             btnBajaM.TabIndex = 10;
@@ -148,7 +157,7 @@
             // btnActivar
             // 
             btnActivar.BackColor = SystemColors.ActiveCaption;
-            btnActivar.Location = new Point(340, 581);
+            btnActivar.Location = new Point(301, 603);
             btnActivar.Name = "btnActivar";
             btnActivar.Size = new Size(139, 29);
             btnActivar.TabIndex = 11;
@@ -158,42 +167,91 @@
             // 
             // btnCursos
             // 
-            btnCursos.Location = new Point(12, 33);
+            btnCursos.BackColor = SystemColors.GradientActiveCaption;
+            btnCursos.Location = new Point(371, 12);
             btnCursos.Name = "btnCursos";
-            btnCursos.Size = new Size(163, 29);
+            btnCursos.Size = new Size(163, 35);
             btnCursos.TabIndex = 12;
             btnCursos.Text = "Cursos";
-            btnCursos.UseVisualStyleBackColor = true;
+            btnCursos.UseVisualStyleBackColor = false;
             btnCursos.Click += btnCursos_Click;
             // 
             // btnPrograma
             // 
-            btnPrograma.Location = new Point(12, 91);
+            btnPrograma.BackColor = SystemColors.GradientActiveCaption;
+            btnPrograma.Location = new Point(7, 12);
             btnPrograma.Name = "btnPrograma";
-            btnPrograma.Size = new Size(163, 29);
+            btnPrograma.Size = new Size(163, 35);
             btnPrograma.TabIndex = 13;
             btnPrograma.Text = "Programas";
-            btnPrograma.UseVisualStyleBackColor = true;
+            btnPrograma.UseVisualStyleBackColor = false;
             btnPrograma.Click += btnPrograma_Click;
             // 
             // btnDocentes
             // 
-            btnDocentes.Location = new Point(197, 33);
+            btnDocentes.BackColor = SystemColors.GradientActiveCaption;
+            btnDocentes.Location = new Point(556, 12);
             btnDocentes.Name = "btnDocentes";
-            btnDocentes.Size = new Size(163, 29);
+            btnDocentes.Size = new Size(163, 35);
             btnDocentes.TabIndex = 14;
             btnDocentes.Text = "Docentes";
-            btnDocentes.UseVisualStyleBackColor = true;
+            btnDocentes.UseVisualStyleBackColor = false;
             btnDocentes.Click += btnDocentes_Click;
             // 
             // btnInscripciones
             // 
-            btnInscripciones.Location = new Point(197, 91);
+            btnInscripciones.BackColor = SystemColors.GradientActiveCaption;
+            btnInscripciones.Location = new Point(192, 12);
             btnInscripciones.Name = "btnInscripciones";
-            btnInscripciones.Size = new Size(163, 29);
+            btnInscripciones.Size = new Size(163, 35);
             btnInscripciones.TabIndex = 15;
             btnInscripciones.Text = "Inscripciones";
-            btnInscripciones.UseVisualStyleBackColor = true;
+            btnInscripciones.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(967, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 20);
+            label1.TabIndex = 16;
+            label1.Text = "Nombres";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(967, 209);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 20);
+            label2.TabIndex = 17;
+            label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(967, 269);
+            label3.Name = "label3";
+            label3.Size = new Size(144, 20);
+            label3.TabIndex = 18;
+            label3.Text = "Cedula de Identidad";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(967, 322);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 20);
+            label4.TabIndex = 19;
+            label4.Text = "Correo Electronico";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(967, 380);
+            label5.Name = "label5";
+            label5.Size = new Size(146, 20);
+            label5.TabIndex = 20;
+            label5.Text = "Número de Telefono";
             // 
             // Form1
             // 
@@ -201,6 +259,11 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
             ClientSize = new Size(1267, 690);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnInscripciones);
             Controls.Add(btnDocentes);
             Controls.Add(btnPrograma);
@@ -241,5 +304,10 @@
         private Button btnPrograma;
         private Button btnDocentes;
         private Button btnInscripciones;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
--- CapaPresentacion/Form1.cs
+++ CapaPresentacion/Form1.cs
@@ -1,4 +1,5 @@
 using CapaNegocios;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace CapaPresentacion
@@ -11,6 +12,9 @@ namespace CapaPresentacion
         public Form1()
         {
             InitializeComponent();
+            txtCorreo.KeyPress += txtCorreo_KeyPress;
+            txtCorreo.Leave += txtCorreo_Leave;
+            txtCorreo.TextChanged += txtCorreo_TextChanged;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -152,6 +156,149 @@ namespace CapaPresentacion
             WF_Docente docente = new WF_Docente();
             docente.Show();
             this.Hide();
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //usamos este KeyPress para validar que solo se puedan ingresar letras y espacios en el campo de nombre
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != ' ' && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (e.KeyChar == ' ')
+            {
+                string texto = ((TextBox)sender).Text;
+                if (texto.Length == 0 || texto.EndsWith(" "))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != ' ' && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            // Evita espacio al inicio o espacios dobles
+            if (e.KeyChar == ' ')
+            {
+                string texto = ((TextBox)sender).Text;
+                if (texto.Length == 0 || texto.EndsWith(" "))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txtCedula_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //En este caso nuestro KeyPress se hace un poco más complejo, debido al formato necesario para la CEDULA
+
+            TextBox txt = (TextBox)sender; //convertimos el sender a un TextBox para poder manipular su texto y la posición del cursor
+            string texto = txt.Text; //variable que nos servira para validar el formato de la cedula y así insertar el guión en el lugar correcto
+            int pos = txt.SelectionStart; //variable que nos servira para saber la posición del cursor y así insertar el guión en el lugar correcto
+
+            // Solo permite números y Backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            // Inserta guión automáticamente
+            if (e.KeyChar != (char)Keys.Back)
+            {
+                if (pos == 3 || pos == 11) // Posición del guión
+                {
+                    txt.Text = texto.Insert(pos, "-"); //inserta el guión en la posición correcta
+                    txt.SelectionStart = pos + 1; //mueve el cursor después del guión
+                }
+            }
+
+            // Limita la longitud máxima a 13 caracteres
+            if (texto.Length >= 13 && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtCorreo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Este es otro keypress que estaremos modificando para que se adapte a lo necesario
+            //para el formato de correo electrónico, permitiendo solo caracteres válidos para un correo
+
+            string caracteresValidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@._-+";
+
+            if (!caracteresValidos.Contains(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtCorreo_Leave(object sender, EventArgs e)
+        {
+            //con este evento validamos el formato del correo electrónico utilizando una expresión regular, mostrando un mensaje de error si el formato es inválido
+
+            TextBox txt = (TextBox)sender; //convertimos el sender a un TextBox para poder manipular su texto y así validar el formato del correo
+
+            // Expresión regular para validar el formato del correo electrónico
+            string patron = @"^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$";
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(txt.Text, patron))
+            {
+                MessageBox.Show("Formato inválido. Use: ejemplo@correo.com",
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                txt.Focus();
+                txt.SelectAll();
+            }
+        }
+
+        private void txtCorreo_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            string patron = @"^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$";
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(txt.Text, patron))
+            {
+                txt.BackColor = Color.LightGreen;  // Correo válido
+            }
+            else
+            {
+                txt.BackColor = Color.LightCoral;  // Correo inválido
+            }
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Este KeyPress es similar al de la cedula, pero con un formato diferente, permitiendo solo números, guiones y paréntesis, y limitando la longitud a 13 caracteres
+
+            TextBox txt = (TextBox)sender;
+            string caracteresValidos = "0123456789()-";
+            string texto = txt.Text;
+
+            if (!caracteresValidos.Contains(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+
+            if (texto.Length >= 13 && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void txtCedula_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
